2de2f3530ef3e7aabc315e705ead1264512ffd23
qwl <qwl@ncmem.com>
2017-07-17 16:06:48 +0800

    更新数据表结构

--- WebRoot/sql/up6_files.sql
+++ WebRoot/sql/up6_files.sql
@@ -1,24 +1,23 @@
-CREATE TABLE `up6_files` (
-  `f_id` int(11) NOT NULL auto_increment,
-  `f_pid` int(11) default '0',
-  `f_pidRoot` int(11) default '0',
-  `f_fdTask` tinyint(1) default '0',
-  `f_fdID` int(11) default '0',
-  `f_fdChild` tinyint(1) default '0',
-  `f_uid` int(11) default '0',
-  `f_nameLoc` varchar(255) default '',
-  `f_nameSvr` varchar(255) default '',
-  `f_pathLoc` varchar(255) default '',
-  `f_pathSvr` varchar(255) default '',
-  `f_pathRel` varchar(255) default '',
-  `f_md5` varchar(40) default '',
-  `f_lenLoc` bigint(19) default '0',
-  `f_sizeLoc` varchar(10) default '0',
-  `f_pos` bigint(19) default '0',
-  `f_lenSvr` bigint(19) default '0',
-  `f_perSvr` varchar(6) default '0%',
-  `f_complete` tinyint(1) default '0',
-  `f_time` timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `f_deleted` tinyint(1) default '0',
+CREATE TABLE IF NOT EXISTS `up6_files` (
+  `f_id` 				char(32) NOT NULL,
+  `f_pid` 				char(32) default '',		/*父级文件夹ID*/
+  `f_pidRoot` 			char(32) default '',		/*根级文件夹ID*/
+  `f_fdTask` 			tinyint(1) default '0',		/*是否是一条文件夹信息*/
+  `f_fdChild` 			tinyint(1) default '0',		/*是否是文件夹中的文件*/
+  `f_uid` 				int(11) default '0',
+  `f_nameLoc` 			varchar(255) default '',	/*文件在本地的名称（原始文件名称）*/
+  `f_nameSvr` 			varchar(255) default '',	/*文件在服务器的名称*/
+  `f_pathLoc` 			varchar(255) default '',	/*文件在本地的路径*/
+  `f_pathSvr` 			varchar(255) default '',	/*文件在远程服务器中的位置*/
+  `f_pathRel` 			varchar(255) default '',
+  `f_md5` 				varchar(40) default '',		/*文件MD5*/
+  `f_lenLoc` 			bigint(19) default '0',		/*文件大小*/
+  `f_sizeLoc` 			varchar(10) default '0',	/*文件大小（格式化的）*/
+  `f_pos` 				bigint(19) default '0',		/*续传位置*/
+  `f_lenSvr` 			bigint(19) default '0',		/*已上传大小*/
+  `f_perSvr` 			varchar(7) default '0%',	/*已上传百分比*/
+  `f_complete` 			tinyint(1) default '0',		/*是否已上传完毕*/
+  `f_time` 				timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `f_deleted` 			tinyint(1) default '0',
   PRIMARY KEY  (`f_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
--- WebRoot/sql/up6_folders.sql
+++ WebRoot/sql/up6_folders.sql
@@ -1,20 +1,20 @@
-CREATE TABLE up6_folders (
-   fd_id 			int(11) NOT NULL auto_increment
-  ,fd_name 			varchar(50) default ''
-  ,fd_pid 			int(11) default '0'
-  ,fd_uid 			int(11) default '0'
-  ,fd_length 		bigint(19) default '0'
-  ,fd_size 			varchar(50) default '0'
-  ,fd_pathLoc 		varchar(255) default ''
-  ,fd_pathSvr 		varchar(255) default ''
-  ,fd_folders 		int(11) default '0'
-  ,fd_files 		int(11) default '0'
-  ,fd_filesComplete int(11) default '0'
-  ,fd_complete 		tinyint(1) default '0'
-  ,fd_delete 		tinyint(1) default '0'
-  ,fd_json 			varchar(20000) default ''
-  ,timeUpload 		timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
-  ,fd_pidRoot		int(11) default '0'
-  ,fd_pathRel		varchar(255) default ''
-  ,PRIMARY KEY  (fd_id)
+CREATE TABLE IF NOT EXISTS `up6_folders` (
+  `fd_id` 				char(32) NOT NULL ,
+  `fd_name` 			varchar(50) default '',
+  `fd_pid` 				char(32) default '',
+  `fd_uid` 				int(11) default '0',
+  `fd_length` 			bigint(19) default '0',
+  `fd_size` 			varchar(50) default '0',
+  `fd_pathLoc` 			varchar(255) default '',
+  `fd_pathSvr` 			varchar(255) default '',
+  `fd_pathRel` 			varchar(255) default '',
+  `fd_folders` 			int(11) default '0',
+  `fd_files` 			int(11) default '0',
+  `fd_filesComplete` 	int(11) default '0',
+  `fd_complete` 		tinyint(1) default '0',
+  `fd_delete` 			tinyint(1) default '0',
+  `fd_json` 			varchar(20000) default '',
+  `timeUpload` 			timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `fd_pidRoot` 			char(32) default '',
+  PRIMARY KEY  (`fd_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
