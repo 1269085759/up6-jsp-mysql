ab22c19aa3e307c5478d1541ff99641f3381c520
qwl <qwl@ncmem.com>
2017-07-23 15:02:29 +0800

    下载表id字段更新

--- WebRoot/sql.down/down_files.sql
+++ WebRoot/sql.down/down_files.sql
@@ -1,7 +1,7 @@
 /*--drop table down_files*/
 CREATE TABLE down_files
 (
- f_id      		int(11) NOT NULL auto_increment    
+ f_id      		char(32) NOT NULL     
 ,f_uid        	int(11) 	DEFAULT '0' 
 ,f_mac        	varchar(50) DEFAULT  '' 
 ,f_nameLoc		varchar(255)DEFAULT ''
@@ -9,10 +9,9 @@ CREATE TABLE down_files
 ,f_fileUrl      varchar(255)DEFAULT '' 	
 ,f_perLoc    	varchar(6) 	DEFAULT '0' 
 ,f_lenLoc    	bigint(19) 	DEFAULT '0' 
-,f_lenSvr		bigint(19) DEFAULT '0'
+,f_lenSvr		bigint(19)  DEFAULT '0'
 ,f_sizeSvr      varchar(10) DEFAULT '0' 
 ,f_complete		tinyint(1)	DEFAULT '0'	
-,f_pidRoot		int(11) 	DEFAULT '0'	
 ,f_fdTask		tinyint(1) 	DEFAULT '0'	
 ,PRIMARY KEY  (`f_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
