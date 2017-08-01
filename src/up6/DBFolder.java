81ef9339648e70822edc1801629e0716324237ab
xfl <xfl@ncmem.com>
2017-08-01 17:56:34 +0800

    上传队列删除文件夹上传任务逻辑完善

--- src/up6/DBFolder.java
+++ src/up6/DBFolder.java
@@ -12,26 +12,22 @@ public class DBFolder {
 
 	static public void Remove(String id,int uid)
 	{
-		StringBuilder sb = new StringBuilder();
-		sb.append("begin ");
-		sb.append("update up6_files set f_deleted=1 where f_id=? and f_uid=?;");
-		sb.append("update up6_files set f_deleted=1 where f_pidRoot=? and f_uid=?;");
-		sb.append("update up6_folders set fd_delete=1 where fd_id=? and fd_uid=?;");
-		sb.append(" end;");		
 		DbHelper db = new DbHelper();
-		PreparedStatement cmd = db.GetCommand(sb.toString());
-		try 
-		{
-			cmd.setString(1, id);
-			cmd.setInt(2, uid);
-			cmd.setString(3, id);
-			cmd.setInt(4, uid);
-			cmd.setString(5, id);
-			cmd.setInt(6, uid);
+		Connection con = db.GetCon();
+		
+		try {
+			con.setAutoCommit(false);
+			Statement stmt = con.createStatement();
+			stmt.addBatch("update up6_files set f_deleted=1 where f_id='" + id + "' and f_uid=" + uid);
+			stmt.addBatch("update up6_files set f_deleted=1 where f_pidRoot='" + id + "' and f_uid=" + uid);
+			stmt.addBatch("update up6_folders set fd_delete=1 where fd_id='" + id + "' and fd_uid=" + uid);
+			stmt.executeBatch();
+			con.commit();
+			stmt.close();
+			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		db.ExecuteNonQuery(cmd);
+		}		
 	}
 }
