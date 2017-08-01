0dba25ca4af3ebfe140e77bf9042dc1e84d6b89a
qwl <qwl@ncmem.com>
2017-08-01 13:45:11 +0800

    下载控件版本号更新

--- WebRoot/down2/js/down.js
+++ WebRoot/down2/js/down.js
@@ -44,7 +44,7 @@ function DownloaderMgr()
 		, "Debug"		: false//调试模式
 		, "LogFile"		: "f:\\log.txt"//日志文件路径。
 		, "Company"		: "荆门泽优软件有限公司"
-		, "Version"		: "1,2,64,51179"
+		, "Version"		: "1,2,65,51249"
 		, "License"		: ""//
 		, "Cookie"		: ""//
 		, "ThreadCount"	: 1//并发数
