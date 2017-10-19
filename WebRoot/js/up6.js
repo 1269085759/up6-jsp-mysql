9733ae30df0bb410c4a379bb6cee46ed6d65986e
xfl <xfl@ncmem.com>
2017-10-19 10:15:37 +0800

    版本号更新：2,7,112,51466

--- WebRoot/js/up6.js
+++ WebRoot/js/up6.js
@@ -87,7 +87,7 @@ function HttpUploaderMgr()
 	this.Config = {
 		  "EncodeType"		: "utf-8"
 		, "Company"			: "荆门泽优软件有限公司"
-		, "Version"			: "2,7,111,51250"
+		, "Version"			: "2,7,112,51466"
 		, "License"			: ""//
 		, "Authenticate"	: ""//域验证方式：basic,ntlm
 		, "AuthName"		: ""//域帐号
